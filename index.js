c96b70d75c157ec9bfbc8363aa857b589463ff27
ABDIRIZACK1 <abdirizack.adan@student.moringaschool.com>
2024-04-15 14:12:02 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here!// Write your code here!
+
+const mainNode = document.querySelector('main#main')
+mainNode.remove()
+
+let newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.textContent = "Abdirizack is the champion"
